38fbd113537fbd7b14b8e8b5032ba18f5239e3ba
Steve C <diceroll123@gmail.com>
2025-03-19 20:44:31 -0400

    🤡🪄 fed the code some metaphysical spaghetti

--- stow.sh
+++ stow.sh
@@ -3,7 +3,7 @@
 # Delete any and all .DS_Store files recursively
 find . -name ".DS_Store" -delete
 
-STOW_FOLDERS="atuin,bat,eza,ghostty,git,lazygit,nvim,starship,yazi,zsh"
+STOW_FOLDERS="atuin,bat,eza,ghostty,git,lazygit,nvim,starship,tmux,yazi,zsh"
 
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g"); do
     echo "Stowing $folder"
