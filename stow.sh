e53bee49c935f215e933364f900abf4f989c3876
Steve C <diceroll123@gmail.com>
2025-04-03 18:00:39 -0400

    add backup for existing .zsh* files

--- stow.sh
+++ stow.sh
@@ -1,9 +1,23 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 
 # Delete any and all .DS_Store files recursively
 find . -name ".DS_Store" -delete
 
 STOW_FOLDERS="atuin,bat,eza,ghostty,git,lazygit,nvim,starship,tmux,yazi,zsh"
+
+# back up any existing .zsh* files
+if [ -f ~/.zshrc ]; then
+    echo "Backing up existing .zshrc to .zshrc.bak"
+    mv ~/.zshrc ~/.zshrc.bak
+fi
+if [ -f ~/.zshenv ]; then
+    echo "Backing up existing .zshenv to .zshenv.bak"
+    mv ~/.zshenv ~/.zshenv.bak
+fi
+if [ -f ~/.zprofile ]; then
+    echo "Backing up existing .zprofile to .zprofile.bak"
+    mv ~/.zprofile ~/.zprofile.bak
+fi
 
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g"); do
     echo "Stowing $folder"
