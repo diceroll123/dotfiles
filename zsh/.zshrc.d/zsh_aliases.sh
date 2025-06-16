161521056bbeb59a330a61e9025843d61a1bab8a
Steve C <diceroll123@gmail.com>
2025-06-15 23:46:03 -0400

    Updated zsh_aliases.sh

--- zsh/.zshrc.d/zsh_aliases.sh
+++ zsh/.zshrc.d/zsh_aliases.sh
@@ -1,8 +1,7 @@
-alias zshconfig="nvim ~/.zshrc"
-alias zshrc="nvim ~/.zshrc"
-alias rc="nvim ~/.zshrc"
-alias reload=". ~/.zshrc"
-alias rl=". ~/.zshrc"
+alias zshconfig="nvim $HOME/.zshrc"
+alias zshrc="nvim $HOME/.zshrc"
+alias rc="nvim $HOME/.zshrc"
+alias rl=". $HOME/.zshrc"
 alias cd="z"
 alias zz="search_with_zoxide"
 alias fdh="fd -H"
@@ -13,18 +12,17 @@ alias jqc='jq | pbcopy'
 alias fj='pbpaste | jqc'
 alias chromekill='ps aux | grep "Google Chrome" | grep "type=renderer" | awk '\''{print $2}'\'' | xargs kill -9'
 alias cmds='tldr --list | fzf --preview "tldr {1} --color always -p osx" --preview-window=right,70% | xargs tldr'
-alias dots="cd ~/dotfiles && nvim"
+alias dots="cd $HOME/dotfiles && nvim"
 alias nv="nvim"
 alias fucking="sudo"
 alias please="sudo"
-alias cdg="cd ~/Documents/Github"
+alias cdg="cd $HOME/Documents/Github"
 alias mkdir='mkdir -p'
 alias pcr="pre-commit run --all-files"
 alias n="nvim"
 
 # if trash is not installed, make an alias for it
-if ! command -v trash &> /dev/null
-then
+if ! command -v trash &>/dev/null; then
   alias trash="/bin/rm"
 else
   alias rm="trash"
@@ -52,9 +50,15 @@ alias dtree="tree -L 3 -a -d -I '.git' --charset X "
 alias catn="cat -n"
 
 # alias for removing failed commands from atuin history
-alias atuindelete='sqlite3 ~/.local/share/atuin/history.db "DELETE FROM history WHERE exit != 0;"'
+alias atuindelete='sqlite3 $HOME/.local/share/atuin/history.db "DELETE FROM history WHERE exit != 0;"'
 
 # Intuitive map function
 # For example, to list all directories that contain a certain file:
 # find . -name .gitattributes | map dirname
 alias map="xargs -n1"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
+
+# Reload the shell (i.e. invoke as a login shell)
+alias reload="exec ${SHELL} -l"
